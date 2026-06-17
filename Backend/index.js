2c90ecae0908098199950308efd53748d9eeba37
Pnch0 <francisco.espinozadi02@gmail.com>
2026-06-16 22:35:52 -0400

    feat: implementar conexion index.js

---
+++ Backend/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+
+import routeUsers from './Routes/routes.users.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/usuarios', routeUsers);
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
