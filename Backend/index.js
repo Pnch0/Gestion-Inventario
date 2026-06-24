5484dc087eef8e5d2d5d097083bb39b6ef5c807e
Pnch0 <francisco.espinozadi02@gmail.com>
2026-06-23 20:32:51 -0400

    feat: modificacion archivo index.js backend

--- Backend/index.js
+++ Backend/index.js
@@ -3,12 +3,14 @@ import express from 'express';
 import cors from 'cors';
 
 import routeUsers from './Routes/routes.users.js';
+import routeProducts from './Routes/routes.products.js'
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', routeUsers);
+app.use('/api/products'. routeProducts);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
