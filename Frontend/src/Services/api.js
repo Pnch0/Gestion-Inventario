ab7903f2ccd00202d5f3ed26f77c6fed818fd691
Pnch0 <francisco.espinozadi02@gmail.com>
2026-07-21 11:40:17 -0400

    feat: creacion LoginService (logica inicio sesion), archivo api.js

--- Frontend/src/Services/api.js
+++ Frontend/src/Services/api.js
@@ -254,8 +254,24 @@ export const roleService = {
             throw error;
         }
     }
+};
 
 
+export const LoginService = {
+    login: async (credentials) =>{
+        const response = await fetch(`${API_URL}/users/login`,{
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(credentials)
+        });
+
+        if (!response.ok){
+            const errorData = await response.json();
+            throw new Error(errorData.message || 'Error al iniciar sesión');
+        }
+
+        return await response.json()
+    }
 };
 
 
