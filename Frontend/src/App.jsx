7d923121904a9a3f3a13963be71743682d5bd456
Pnch0 <francisco.espinozadi02@gmail.com>
2026-07-27 11:28:11 -0400

    feat: creacion de nuevos archivos (ProductsPage.jsx - ProductsPage.css), ademas de modificacion del archivo App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import MainLayout from './Components/Layouts/MainLayout/MainLayout.jsx';
 import MainPage from './Pages/MainPage/MainPage.jsx';
 import LoginPage from './Pages/Login/LoginPage.jsx';
 import UsersPage from './Pages/Users/UsersPage.jsx';
+import ProductsPage from './Pages/Products/ProductsPage.jsx';
 import { Toaster } from 'react-hot-toast';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route element={<MainLayout />}>
           <Route path="/main-page" element={<MainPage />} />
           <Route path="/users-page" element={<UsersPage />} />
+          <Route path="/list-page" element={<ProductsPage />} />
         </Route>
 
         <Route path="*" element={<h2>Página no encontrada</h2>} />
---
+++ Frontend/src/Pages/Products/ProductsPage.jsx
@@ -0,0 +1,13 @@
+import './ProductsPage.css';
+
+function ProductsPage(){
+
+    return(
+        <>
+        <h1>Products Page</h1>
+        </>
+    )
+}
+
+
+export default ProductsPage;
