da01a26bda0c08a17be3004ca848607d5822fa7b
Pnch0 <francisco.espinozadi02@gmail.com>
2026-07-29 12:24:00 -0400

    feat: implementacion y diseño del componente SalesList en la pagina SalesPage, archivo SalesPage.jsx - SalesList.jsx - SalesPage.css

--- Frontend/src/Components/Sales/SalesList.jsx
+++ Frontend/src/Components/Sales/SalesList.jsx
@@ -1,12 +1,46 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { saleService } from "../../Services/api.js";
+import { ClipLoader } from "react-spinners";
+import { FaEdit, FaTrashAlt, FaBox } from "react-icons/fa";
+import toast, { Toaster } from 'react-hot-toast';
 
 function SalesList(){
     const [sales, setSales] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState('');
     const [expandedSales, setExpandedSales] = useState({});
+
+    const toastStyles = {
+        exito: {
+            duration: 4000,
+            position: 'top-center',
+            style: {
+                border: '1px solid #BBF7D0',
+                padding: '16px',
+                color: '#166534',
+                background: '#EDFCF2',
+            },
+            iconTheme: {
+                primary: '#15803D',
+                secondary: '#EDFCF2',
+            },
+        },
+        error: {
+            duration: 4000,
+            position: 'top-center',
+            style: {
+                border: '1px solid #FECDD3',
+                padding: '16px',
+                color: '#991B1B',
+                background: '#FFF1F2',
+            },
+            iconTheme: {
+                primary: '#991B1B',
+                secondary: '#FFF1F2',
+            },
+        }
+    };
 
     const fetchSales = async () =>{
         try{
@@ -39,14 +73,18 @@ function SalesList(){
         return new Date(dateString).toLocaleDateString('es-ES', options);
     };
 
-    if (loading) return <div>Cargando Ventas...</div>;
+    if (loading) return (
+        <div style={{ display: 'flex', justifyContent: 'center', padding: '50px' }}>
+            <ClipLoader color="#3da35d" loading={loading} size={50} />
+        </div>
+    );
+
     if (error) return <div style={{ color: 'red' }}>Error: {error}</div>;
 
 
     return(
         <>
         <div className="SalesList-Contenedor">
-            <h2>Lista de Ventas</h2>
             {sales.length === 0 ? (
                 <p>No hay ventas registradas</p>
             ) : (
@@ -57,6 +95,15 @@ function SalesList(){
                             <span>{sale.rut || 'Sin RUT'}</span>
                             <span>{formatDate(sale.fecha_hora)}</span>
                             <span>${sale.total_general}</span>
+
+                            <div className="Acciones-Botones">
+                                <button type="button" onClick={(e) => handleEdit(e, product)}>
+                                    <FaEdit className="Icono-Acciones"/>
+                                </button>
+                                <button type="button" onClick={(e) => handleDelete(e, product.producto_id)}>
+                                    <FaTrashAlt className="Icono-Acciones"/>
+                                </button>
+                            </div>
                             
                         </li>
                     ))}
--- Frontend/src/Pages/Sales/SalesPage.jsx
+++ Frontend/src/Pages/Sales/SalesPage.jsx
@@ -55,7 +55,7 @@ function SalesPage(){
                 </div>
 
                 <div className="Contenedor-ListadoVentas">
-
+                    <SalesList refreshTrigger={refreshTrigger}/>
                 </div>
 
             </div>
