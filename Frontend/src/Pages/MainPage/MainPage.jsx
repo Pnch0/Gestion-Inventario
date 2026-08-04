0a1565de360526cea96642e478912470b1422436
Pnch0 <francisco.espinozadi02@gmail.com>
2026-08-04 13:17:05 -0400

    feat: creacion de archivos MainPage.jsx - MainPage.css junto con su estructura y diseño

--- Frontend/src/Pages/MainPage/MainPage.jsx
+++ Frontend/src/Pages/MainPage/MainPage.jsx
@@ -1,14 +1,35 @@
+import './MainPage.css';
+import { useState, useEffect } from 'react';
+import { IoIosWarning } from "react-icons/io";
+import { FaShoppingCart, FaFire } from "react-icons/fa";
 
+function MainPage() {
 
-function MainPage(){
-
-    return(
+    return (
         <>
-        
-        <h1>MainPage</h1>
-        
+        <div className="Contenedor-MainPage">
+            <div className="ContenedorSecciones-MainPage">
+                <div className="ContenedorSuperior-MainPage">
+                    <div className="Contenedor-GananciasDia">
+                        <h1>Dinero Ingresado el día de hoy: </h1>
+                    </div>
+                </div>
+                <div className="ContenedorInferior-MainPage">
+                    <div className="Contenedor-UltimasVentas">
+                        <h2><FaShoppingCart  className='Icono-Shop'/>Últimas 5 Ventas</h2>
+                    </div>
+                    <div className="Contenedor-StockFaltante">
+                        <h2><IoIosWarning className='Icono-Warning' />Stock Faltante</h2>
+                    </div>
+                    <div className="Contenedor-MasVendidos">
+                        <h2><FaFire className='Icono-Fire'/>Productos más Vendidos</h2>
+                    </div>
+                </div>
+
+            </div>
+        </div>
         </>
-    )
+    );
 }
 
 export default MainPage;
